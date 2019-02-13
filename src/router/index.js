936ae18598bd47119ea8ba9069f354be74942a11
czy583139 <chenzhendx@qq.com>
2019-02-13 23:10:05 +0800

    登录的路由配置完毕

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import login from '@/components/login.vue' //由webpack模板提供这么写法
 
 Vue.use(Router)
 
@@ -8,5 +9,9 @@ export default new Router({
     path: '/',
     name: 'home'
 
+  }, {
+    path: '/login',
+    name: 'login',
+    component: login
   }]
 })
