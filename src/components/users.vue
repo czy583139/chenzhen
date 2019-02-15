94407740a2abd334c6e034d4ab2485d2bd3f82dc
czy583139 <chenzhendx@qq.com>
2019-02-15 22:16:11 +0800

    面包屑导航和搜索框制作完毕

--- src/components/users.vue
+++ src/components/users.vue
@@ -1,13 +1,42 @@
 <template>
-  <el-card class="box-card"></el-card>
+  <el-card class="box-card">
+    <!-- 面包写导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-row class="search">
+      <el-col>
+        <!-- 搜索框 -->
+        <el-input placeholder="请输入内容" v-model="query" class="searchInput">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="success" plain>添加用户</el-button>
+      </el-col>
+    </el-row>
+  </el-card>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      query: ""
+    };
+  }
+};
 </script>
 
 <style>
 .box-card {
   height: 100%;
+}
+.search {
+  margin-top: 20px;
+}
+.searchInput {
+  width: 400px;
 }
 </style>
